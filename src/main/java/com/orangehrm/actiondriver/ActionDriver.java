2acbaf1cd6dea6f1b93b665abf230ac3073d0d28
MaheshH91 <holkarmahesh11@gmail.com>
2026-03-03 13:24:28 +0530

    code refinement

--- src/main/java/com/orangehrm/actiondriver/ActionDriver.java
+++ src/main/java/com/orangehrm/actiondriver/ActionDriver.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.orangehrm.base.BaseClass;
 import com.orangehrm.utilities.ExtentManager;
